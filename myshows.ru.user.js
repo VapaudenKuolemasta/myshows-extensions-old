08b145c650913f69c511031f954974b4adea9357
Vapauden Kuolemasta <kuolemasta@mail.ru>
2014-11-26 12:18:19 +0400

    Новая версия под новый интерфейс

--- myshows.ru.user.js
+++ myshows.ru.user.js
@@ -1,174 +1,195 @@
 ﻿// ==UserScript==
-// @id 				myshows.ru
-// @name 			myshows.ru
-// @version 		3.0.1
-// @description 	Добавляет ссылки на поиск торрента и субтитров, плюс прямую ссылку на магнет. Добавляет фавикон.
-// @include 		http://myshows.ru/*
-// @match 			http://myshows.ru/*
-// @resource 		myshows-css https://raw.githubusercontent.com/VapaudenKuolemasta/myshows.ru-userscript/master/myshows.ru.css
+// @id 				myshows.me
+// @name 			myshows.me
+// @version 		4.0
+// @description 	Добавляет ссылки на поиск торрента и субтитров, плюс прямую ссылку на магнет.
+// @include 		http://myshows.me/*
+// @match 			http://myshows.me/*
 // @homepageURL 	https://github.com/VapaudenKuolemasta/myshows.ru-userscript
 // @updateURL 		https://github.com/VapaudenKuolemasta/myshows.ru-userscript/raw/master/myshows.ru.user.js
 // @grant 			GM_addStyle
-// @grant 			GM_getResourceText
 // @grant 			GM_xmlhttpRequest
 // ==/UserScript==
 
 // Сделать макет ридми
-// Обновлять раз в 10-15 минут (?)
-// Подписывать размер релиза во всплывающую подсказку
+// Обновлять раз в 10-15 минут, не останавливать аякс совсем, просто запускать его периодически
+// Размер релиза форматировать типа 9 999.99
 // Убрать нафиг файл со стилями, лишний гемор
-// Заполнять магнеты сверху вниз
 
-GM_addStyle(GM_getResourceText('myshows-css'));
+// Херется скрипт, если отметил серию просмотреной в процессе парсинга
+// Небольшую менюшку с настройками и возможностью прикрутить свой торент трекер
 
-var param = '720p';
+param = '720p';
+queue = {};
+threads = 5;
+current_threads = 0;
 
 function addMainIcons(){
-	var bss_seri = document.getElementsByClassName('bss_seri');
-	var count = bss_seri.length;
-	for (i=0; i<count; i++) {
-		var episode_arr = bss_seri[i].textContent.split('x');
-		var episode_str = 'S'+(episode_arr[0]>9?episode_arr[0]:'0'+episode_arr[0])+'E'+(episode_arr[1]>9?episode_arr[1]:'0'+episode_arr[1]);
-
-		if(bss_seri[i].parentElement.parentElement.parentElement.parentElement.previousElementSibling != null){
-			var name = bss_seri[i].parentElement.parentElement.parentElement.parentElement.previousElementSibling
-			while(name.tagName == 'DIV'){
-				name = name.previousElementSibling;
-			} 
-			name = name.childNodes[1] != null?name.childNodes[1].textContent.substr(3):name.textContent;
-			var href = name+'+'+episode_str+'+'+param;
-
-			var newDiv = document.createElement('span');
-			newDiv.innerHTML =
-			'<span class="magnet">'+
-				'<img class="fv_icon" src="http://myshows.ru/shared/images/ajax-loader.gif" title="Поиск magnet-ссылки">'+
-			'</span>'+
-			'<a class="main_link" href="https://thepiratebay.se/search/'+href+'/0/3/0" target="_blank">'+
-				'<img class="fv_icon" src="http://thepiratebay.se/static/img/tpblogo_sm_ny.gif" title="Искать серию на ThePirateBay">'+
-			'</a> '+
-			'<a href="https://www.google.com/search?q='+href+'+torrent" target="_blank">'+
-				'<img class="fv_icon" src="https://www.google.com/images/google_favicon_128.png" title="Искать серию в Google">'+
-			'</a> '+
-			'<a href="http://www.addic7ed.com/search.php?search='+href+'" target="_blank">'+
-				'<img src="http://cdn.addic7ed.com/favicon.ico" title="Искать субтитры на Addic7ed">'+
-			'</a> '+
-			'<a href="http://notabenoid.com/search?t='+name+'" target="_blank">'+
-				'<img src="http://notabenoid.com/i/favicon.ico" title="Искать субтитры на Notabenoid">'+
-			'</a> ';
-
-			var bss_link = document.getElementsByClassName('bss_link')[i];
-			if(bss_link.childNodes[0]!= null && bss_link.childNodes[0].tagName == 'A'){
-				bss_link.insertBefore(newDiv, bss_link.childNodes[0]);
-			} else if(bss_link.childNodes[0]== null) {
-				bss_link.appendChild(newDiv);
-			}	
+	var div_seasonBlockBody = document.querySelectorAll('.seasonBlockBody > table > tbody > tr');
+	var count = div_seasonBlockBody.length;
+	for( i=0; i<count; i++ ){
+	
+		/* BOF Достаем название сериала */
+		var serial_name = div_seasonBlockBody[i].parentElement.parentElement.parentElement.parentElement.previousElementSibling;
+		
+		// Нужно на случай нескольких сезонов
+		while(serial_name.tagName != 'P'){ 
+			serial_name = serial_name.previousElementSibling;
+		} 
+		
+		// Если нет не русского названия
+		if( serial_name.innerHTML == '' ){ 
+			serial_name = serial_name.previousElementSibling.childNodes[0].childNodes[0].textContent;
+		}else{
+			serial_name = serial_name.childNodes[0].textContent;
 		}
+		/* EOF Достаем название сериала */
+		
+		/* BOF Достаем сезон и эпизод */
+		var tmp = div_seasonBlockBody[i].childNodes[1].textContent.split('x');
+		var season = tmp[0]>9?tmp[0]:'0'+tmp[0];
+		var episode = tmp[1]>9?tmp[1]:'0'+tmp[1];
+		var season_episode = 's'+season+'e'+episode;
+		/* EOF Достаем сезон и эпизод */
+		
+		var newTd = document.createElement('td');
+		newTd.className = 'torrentLinks';
+		newTd.innerHTML =
+		'<div class="buttonPopup _download red">'+
+            '<ul>'+
+				'<li>'+
+					'<a target="_blank" href="https://thepiratebay.se/search/'+serial_name+'+'+season_episode+'+'+param+'/0/3/0" rel="nofollow external">'+
+						'<img alt="img" class="fv_icon" src="http://thepiratebay.se/static/img/tpblogo_sm_ny.gif" title="Искать '+serial_name+' '+season_episode+' '+param+' на ThePirateBay">'+
+						'Искать '+serial_name+' '+season_episode+' '+param+' на ThePirateBay'+
+					'</a>'+
+				'</li>'+
+				'<li>'+
+					'<a target="_blank" href="https://www.google.com/search?q='+serial_name+'+'+season_episode+'+'+param+'" rel="nofollow external">'+
+						'<img alt="img" class="fv_icon" src="https://www.google.com/images/google_favicon_128.png" title="Искать '+serial_name+' '+season_episode+' '+param+' на ThePirateBay">'+
+						'Искать '+serial_name+' '+season_episode+' '+param+' в Google'+
+					'</a>'+
+				'</li>'+
+				'<li>'+
+					'<a target="_blank" href="http://www.addic7ed.com/search.php?search='+serial_name+'+'+season_episode+'+'+param+'" rel="nofollow external">'+
+						'<img alt="img" class="fv_icon" src="http://cdn.addic7ed.com/favicon.ico" title="Искать '+serial_name+' '+season_episode+' '+param+' на ThePirateBay">'+
+						'Искать субтитры к '+serial_name+' '+season_episode+' на Addic7ed'+
+					'</a>'+
+				'</li>'+
+				'<li class="magnet">'+
+					'<a target="_blank" href="https://thepiratebay.se/search/'+serial_name+'+'+season_episode+'+'+param+'/0/3/0" rel="nofollow external">'+
+						'<img alt="img" class="fv_icon" src="http://myshows.me/shared/img/vfs/ajax-loader.gif" title="Ищу магнет к '+serial_name+' '+season_episode+' на ThePirateBay">'+
+						'Ищу магнет к '+serial_name+' '+season_episode+' на ThePirateBay'+
+					'</a>'+
+				'</li>'+
+			'</ul>'+
+		'</div>';
+		div_seasonBlockBody[i].appendChild(newTd);
+		queue[div_seasonBlockBody[i].getAttribute('data-id')] = serial_name+'+'+season_episode+'+'+param;
 	}
 }
 
-var magnet = {
-	// Очередь запросов
-	queue : [],
-	
-	// Головная функция получения магнета
-	addMagnet:function(){
-		var main_link = document.getElementsByClassName('main_link');
-		var count = main_link.length;
-		this.queue = [];
-		for (i=0; i<count; i++) {
-			this.queue.push( main_link[i].getAttribute('href') );
-		}
-		this.getContent();
-	},
-	
-	// Рекурсивный метод отправляющий запрос
-	getContent:function(){
-		var _this = this;
-		setTimeout( function(){
-			GM_xmlhttpRequest({
-				method : "GET",
-				url : _this.queue.pop(),
-				onload : function( x ){
-					if( _this.exchangeLinks( _this.parseContent( x ), _this.queue.length ) ){
-						if ( _this.queue.length > 0 ) _this.getContent();
+function getMagnetPage( episode_id, episode_href ){
+	delete queue[episode_id];
+	setTimeout( function(){
+		GM_xmlhttpRequest({
+			method : "GET",
+			url : 'https://thepiratebay.se/search/'+episode_href+'/0/3/0',
+			onload : function( msg ){
+				if( msg ){
+					var parsingResult = parseMagnetPage( msg );	
+					if( parsingResult !== 0 ){
+						var tmp = document.querySelector('tr[data-id="'+episode_id+'"] li.magnet a');
+						if( tmp != null ){
+							tmp.setAttribute('href', parsingResult.link );
+							tmp.childNodes[0].setAttribute('src','https://thepiratebay.se/static/img/icon-magnet.gif');
+							tmp.childNodes[1].textContent = '('+parsingResult.size+' MB) '+parsingResult.name;
+						}
 					}
 				}
-			});
-		},	0 );
-	},
-	
-	// Заменяет лоадеры магнетами или черепом с косятми, если магнет не найден
-	exchangeLinks:function( answer, index ){
-		document.getElementsByClassName('magnet')[ index ].innerHTML = 
-			answer.length>2?
-			'<a href="'+answer+'" ><img src="https://thepiratebay.se/static/img/icon-magnet.gif" title="Скачать magnet с ThePirateBay"></a>':
-			'<img src="https://thepiratebay.se/static/img/trusted.png" title="Ничего не найдено на ThePirateBay">';
-		return true;
-	},
-	
-	// Тут распарсиваются данные, полученные запросом
-	parseContent:function( answer ){
-		var responseDiv = document.createElement('div');
-		responseDiv.innerHTML = answer.responseText;
-		if( responseDiv.getElementsByClassName('detLink') != null){
-			var links = responseDiv.getElementsByClassName('detLink');
-			var descs = responseDiv.querySelectorAll('font.detDesc');
-			var count = links.length;
-			var webdl_link_size = 0;
-			var hd_link_size = 0;
-			var webdl_link = 0;
-			var hd_link = 0;
-			for (i=0; i<count; i++) {
-				var cur_size = this.toMiB( descs[i].childNodes[0].textContent )
-				if( links[i].text.indexOf('WEB-DL') != -1 ){
-					if( cur_size>webdl_link_size ){
-						webdl_link_size = cur_size;
-						webdl_link = links[i].parentElement.parentElement.querySelector('a[href^=magnet]').getAttribute('href');
-					}
-				}else{
-					if( cur_size > hd_link_size ){
-						hd_link_size = cur_size;
-						hd_link = links[i].parentElement.parentElement.querySelector('a[href^=magnet]').getAttribute('href');
-					}
+				
+				var tmp_ei = Object.keys(queue).shift();
+				var tmp_eh = queue[tmp_ei]
+				if( tmp_ei ){
+					getMagnetPage( tmp_ei, tmp_eh );
 				}
 			}
-			return webdl_link_size>0?webdl_link:hd_link;
-		}else{
-			return 0;
-		}
-	},
+		});
+	},	0 );
+}
+
+function parseMagnetPage( pageContent ){
+	var responseDiv = document.createElement('div');
+	responseDiv.innerHTML = pageContent.responseText;
 	
-	// Функция определения размера релиза
-	toMiB:function( size ){
-		var regexp = /(\d+(\.\d+|))\s(GiB|KiB|MiB)/;
-		var res = regexp.exec( size );
-		if( res == null ) return 0;
-		switch ( res[3] ) {
-			case 'GiB': return ( +res[1] * 1000 );
-			case 'KiB': return ( +res[1] * 0.001 );
-			case 'MiB': return ( +res[1] );
-			default: return 0;
+	if( responseDiv.getElementsByClassName('detLink').length > 0 ){
+		
+		var cur_size = 0, index = 0;
+		
+		var tr_list = responseDiv.querySelectorAll('#searchResult > tbody > tr');
+		for (i=0; i<tr_list.length; i++) {
+			var new_size = toMib( tr_list[i].children[1].querySelector('.detDesc').childNodes[0].textContent ); 
+			if( new_size > cur_size ){ 
+				index = i;
+				cur_size = new_size;
+			}
 		}
+		
+		var r = {
+			name : tr_list[index].querySelector('.detName a').childNodes[0].textContent,
+			link : tr_list[index].children[1].children[1].getAttribute('href'),
+			size : cur_size
+		}
+		
+		return r;
+	}else{
+		return 0;
 	}
 }
 
-var icon = document.createElement('link');
-icon.setAttribute('type', 'image/x-icon');
-icon.setAttribute('rel', 'shortcut icon');
-icon.setAttribute('href', 'http://myshows.ru/shared/images/header/logo-tv.gif');
-document.getElementsByTagName('head')[0].appendChild(icon);
+function toMib( size ){
+	var regexp = /(\d+(\.\d+|))\s(GiB|KiB|MiB)/;
+	var res = regexp.exec( size );
+	if( res == null ) return 0;
+	switch ( res[3] ) {
+		case 'GiB': return ( +res[1] * 1000 );
+		case 'KiB': return ( +res[1] * 0.001 );
+		case 'MiB': return ( +res[1] );
+		default: return 0;
+	}
+}
 
+// function addSoonBlock(){
+	// setTimeout( function(){
+		// GM_xmlhttpRequest({
+			// method : "GET",
+			// url : 'http://myshows.me/',
+			// onload : function( answer ){
+				// var responseDiv = document.createElement('div');
+				// responseDiv.innerHTML = answer.responseText;
+				// var seasonBlock = responseDiv.getElementsByClassName('seasonBlock');
+				// document.getElementsByTagName('h1')[0].parentElement.insertBefore(seasonBlock[0], document.getElementsByTagName('h1')[0]);
+				// document.getElementsByTagName('h1')[0].parentElement.insertBefore(seasonBlock[1], document.getElementsByTagName('h1')[0]);
+			// }
+		// });
+	// },	0 );
+// }
 
 addMainIcons();
-magnet.addMagnet();
+for( i=0; i<threads; i++ ){
+	getMagnetPage( Object.keys(queue).shift(), queue[Object.keys(queue).shift()] );
+}
+// addSoonBlock();
 
-document.addEventListener( 
-	'DOMNodeRemoved', 
-	function( e ){
-		if( e.target.classList[0].indexOf('rside') >= 0 ) {
-			addMainIcons();
-			magnet.addMagnet();
-		}
-	}, 
-	false 
+GM_addStyle(
+	'.fv_icon{'+
+		'width: 13px;'+
+		'margin: -5px 5px 0 0'+
+	'}'+
+	'.torrentLinks{'+
+		'width:24px;'+
+	'}'+
+	'.buttonPopup.red{'+
+		'background-color:red;'+
+	'}'+
+	''
 );
